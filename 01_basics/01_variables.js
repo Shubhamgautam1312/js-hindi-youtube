d092d059a4f90275c9b788c59f2f33ff85d3712a
Shubham Gautam <166021036+Shubhamgautam1312@users.noreply.github.com>
2024-07-08 14:29:25 +0000

    Lec 4

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId ="32466"
+let  accountEmail="gautamshubham.com"
+var accountPassword="4364vf"
+accountCity="jaipur"
+let accountState;
+
+// accountId="5454"  //  Not Allowed
+
+/*
+Prefer Not to Use Var
+Because of issue in block scope and functional scope
+*/
+
+accountEmail="dgh.com"
+accountPassword="546hgh"
+accountCity="Noida"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
