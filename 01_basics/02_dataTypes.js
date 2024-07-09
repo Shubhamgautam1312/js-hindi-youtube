ea1629086475677937c0b8ab90f705ed957f5490
Shubham Gautam <166021036+Shubhamgautam1312@users.noreply.github.com>
2024-07-09 14:43:26 +0000

    Data Types Lec-5

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+//"use strict";  // Treat all JS code as a newer version
+
+// alert(3 + 3)  //we are using nodejs, not browser //for popup
+
+console.log(3
+    +3
+)  //code readabbility should be high
+
+let name= "Shubahm"
+let age= 18
+let isLoggedIn= true
+let state;
+
+// number => 2 to power 53
+// big int
+// string => ""
+// boolean => true/false
+// null=> standalone value // type-object
+// undefined => typeof state;
+//
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // unefined
