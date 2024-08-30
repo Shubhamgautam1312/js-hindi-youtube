19a1aa0f88071e502680bc6b47a4403772092ecf
Shubham Gautam <166021036+Shubhamgautam1312@users.noreply.github.com>
2024-08-30 16:53:56 +0000

    map ,filter and reduce

---
+++ 05_iterations/nine.js
@@ -0,0 +1,35 @@
+myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc,currval) {
+//    console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0 )
+
+
+// const myTotal = myNums.reduce( (acc,currval) => acc + currval,0)
+// console.log(myTotal);
+
+const shopingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 5999
+    },
+    {
+        itemName: "movile dev course",
+        price:5999
+    },
+    {
+        itemName: "data science course",
+        price:12999
+    }
+]
+   
+
+
+const totalPrice = shopingCart.reduce((acc,item) => acc + item.price,0)
+
+console.log(totalPrice);
